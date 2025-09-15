383ff68d44209ae555c80a55b514b6cf5db352b0
Krishna171205 <sshriyansh827@gmail.com>
2025-09-15 23:22:30 +0530

    deleting user

--- src/pages/admin/page.tsx
+++ src/pages/admin/page.tsx
@@ -43,7 +43,7 @@ interface Consultation {
 
 const AdminDashboard = () => {
   const navigate = useNavigate();
-  const [user,setUser] = useState<any>(null);
+  const [setUser] = useState<any>(null);
   const [loading, setLoading] = useState(true);
   const [activeTab, setActiveTab] = useState('properties');
   const [showAddProperty, setShowAddProperty] = useState(false);
